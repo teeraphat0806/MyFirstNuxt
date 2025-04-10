d005276394503976b4ec5733b47daec7c34a4fcf
Teeraphat Jirapasnitilet <110732968+teeraphat0806@users.noreply.github.com>
2025-04-10 11:17:30 +0700

    fix sidebar position fix and overflow-y 2.Update from doc3

--- ckc/components/layout/Sidebar.vue
+++ ckc/components/layout/Sidebar.vue
@@ -1,25 +1,24 @@
 <template>
   <aside
-    class="group hidden md:block md:absolute top-0 left-0 h-full w-18 hover:w-68 transition-all duration-400 ease-in-out bg-gradient-to-b from-[#40B59F] to-[#F5CC3B] text-white p-2 z-10 "
+    class="group hidden md:block fixed top-0 left-0 h-screen w-18 hover:w-68 transition-all duration-400 ease-in-out bg-gradient-to-b from-[#40B59F] to-[#F5CC3B] text-white z-10 overflow-y-auto"
   >
-    <div class="flex p-1">
+    <div class="flex p-3">
       <div
         class="mt-2 h-9 w-9 rounded-full ml-2 animate-pulse bg-gray-400 mr-2 group-hover:h-9 w-9 hover:duration-300 ease-in-out"
       ></div>
       <div
-        class="grid-cols-1 font-semibold text-lx overflow-hidden max-w-0 opacity-0 group-hover:max-w-full group-hover:opacity-100 transition-all duration-300 ease-in-out whitespace-nowrap"
+        class="grid-cols-1 font-medium text-[1.1rem] overflow-hidden max-w-0 opacity-0 group-hover:max-w-full group-hover:opacity-100 transition-all duration-300 ease-in-out whitespace-nowrap"
       >
-        <h2>ระบบตรวจติดตาม</h2>
-        <h2>การให้ความช่วยเหลือ</h2>
+        <p>ระบบตรวจติดตาม</p>
+        <p>การให้ความช่วยเหลือ</p>
       </div>
     </div>
-
-    <nav class="flex flex-col space-y-2 mt-5">
+    <hr class="w-full overflow-auto space-x-0" />
+    <nav class="flex flex-col space-y-2 mt-5 p-2">
       <div v-for="data in datas" :key="data.id">
         <NuxtLink
           :to="data.nav"
-          class="text-gray-200 hover:text-white hover: px-3 py-2 rounded flex text-xl"
-        style="hover: text-shadow: 0 0 3px #fff, 0 0 5px #fff;"
+          class="text-gray-200 hover:text-white px-3 py-1 rounded flex text-xl"
         >
           <UButton
             class="text-white mr-5"
@@ -41,19 +40,19 @@
 
 <script setup lang="ts">
 const datas = [
-  { icon: "i-mdi-plus-box-outline", title: "สร้างรายการ", nav: "/documents", id: 1 },
-  { icon: "i-mdi-folder-outline", title: "รายการทั้งหมด", nav: "/documents", id: 2 },
-  { icon: "i-mdi-pencil-outline", title: "จัดการชุดเอกสาร", nav: "/documents", id: 3 },
-  { icon: "i-mdi-view-dashboard-outline", title: "ข้อมูลสถิติ", nav: "/documents", id: 4 },
-  { icon: "i-mdi-file-document-outline", title: "รายงาน", nav: "/documents", id: 5 },
-  { icon: "i-mdi-book-open-outline", title: "คู่มือการใช้งาน", nav: "/documents", id: 6 },
-  { icon: "i-mdi-cog-outline", title: "การตั้งค่า", nav: "/documents", id: 7 },
+  { icon: "ic:round-plus", title: "สร้างรายการ", nav: "/documents", id: 1 },
+  { icon: "material-symbols:folder-outline", title: "รายการทั้งหมด", nav: "/documents", id: 2 },
+  { icon: "solar:document-add-outline", title: "จัดการชุดเอกสาร", nav: "/documents", id: 3 },
+  { icon: "uil:statistics", title: "ข้อมูลสถิติ", nav: "/documents", id: 4 },
+  { icon: "mingcute:document-line", title: "รายงาน", nav: "/documents", id: 5 },
+  { icon: "iconoir:book", title: "คู่มือการใช้งาน", nav: "/documents", id: 6 },
+  { icon: "clarity:cog-line", title: "การตั้งค่า", nav: "/documents", id: 7 },
 ];
 </script>
 
 <style lang="scss" scoped>
 aside {
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   height: 100%;
--- ckc/pages/doc3.vue
+++ ckc/pages/doc3.vue
@@ -123,15 +123,13 @@
 
             <UFormField name="announceDate" class="w-full" required>
               <template #label>
-              
-                  <span class="text-[1rem] text-black">วันที่ผ่านการลงรับ</span>
-                  <span
-                    class="hidden group-hover:block text-[1rem] text-black bg-white p-1 rounded shadow-md"
-                    style="top: 100%; left: 0"
-                  >
-                    เอกสารGFMIS (75วัน)
-                  </span>
-            
+                <span class="text-[1rem] text-black">วันที่ผ่านการลงรับ</span>
+                <span
+                  class="hidden group-hover:block text-[1rem] text-black bg-white p-1 rounded shadow-md"
+                  style="top: 100%; left: 0"
+                >
+                  เอกสารGFMIS (75วัน)
+                </span>
               </template>
               <DatePicker
                 v-model="formState.announceDate"
@@ -171,6 +169,108 @@
               </UFormField>
             </UFormField>
           </div>
+          <div class="gap-4 mt-4">
+            <UFormField label="ชั้นความเร็ว">
+              <URadioGroup
+                v-model="formState.urgentLevel"
+                orientation="horizontal"
+                default-value="NORMAL"
+                :items="urgent"
+              />
+              <p class="text-[#FFB800] mt-2">
+                *หมายเหตุ : ถ้าไม่ระบุชั้นของความเร็วเอกสาร คือ เอกสารปกติ
+              </p>
+            </UFormField>
+          </div>
+          <div class="">
+            <p class="text-2xl font-bold mt-5">ข้อมูลภัย</p>
+            <hr class="border-2 border-black" />
+            <div
+              class="mt-5 space-y-5 md:space-y-0 md:grid md:grid-cols-4 gap-4"
+            >
+              <UFormField name="announceDate" class="w-full">
+                <template #label>
+                  <span class="text-[1rem] text-black">ห้วงเกิดภัย</span>
+                </template>
+                <DatePicker
+                  v-model="formState.announceDate"
+                  :max-date="new Date()"
+                  placeholder="เลือกวันที่"
+                  class="w-full"
+                />
+              </UFormField>
+              <UFormField name="announceDate" class="w-full">
+                <template #label>
+                  <span class="text-[1rem] text-black">ขอขยายเวลาถึง</span>
+                </template>
+                <DatePicker
+                  v-model="formState.announceDate"
+                  :max-date="new Date()"
+                  placeholder="เลือกวันที่"
+                  class="w-full"
+                />
+              </UFormField>
+            </div>
+          </div>
+          <div class="">
+            <p class="text-2xl font-bold mt-5">สถานที่ดำเนินการ</p>
+            <hr class="border-2 border-black" />
+            <div
+              class="mt-5 space-y-5 md:space-y-0 md:grid md:grid-cols-4 gap-4"
+            ></div>
+          </div>
+          <div class="">
+            <p class="text-2xl font-bold mt-5">รายการช่วยเหลือ</p>
+            <hr class="border-2 border-black" />
+            <div
+              class="mt-5 space-y-5 md:space-y-0"
+            >
+              <UFormField required>
+              <template #label>
+                <span class="text-[0.9rem] text-black">หลักเกณฑ์</span>
+              </template>
+              <UFormField>
+                 <USelect
+              v-model="formState.fiscalYear"
+              class="w-full"
+              :items="years.map((y) => ({ ...y, value: String(y.value) }))"
+              placeholder="กรุณาเลือก"
+            />
+              </UFormField>
+            </UFormField>
+            </div>
+          </div>
+          <div class="">
+            <p class="text-2xl font-bold mt-5">ค่าใช้จ่าย</p>
+            <hr class="border-2 border-black" />
+            <div
+              class="mt-5 space-y-5 md:space-y-0 md:grid md:grid-cols-4 gap-4"
+            >
+            <div v-for="(item, index) in items2" :key="index">
+              <UFormField v-if="index==0" :error="errors.fiscalYear" required>
+                <template #label>
+                  <div class="group relative">
+                  <span class="text-[1rem] text-black">{{ item.label }}</span>
+                  <span class="mt-1">   </span>
+                  </div>
+                </template>
+                <UFormField :error="item.error">
+                  <UInput placeholder="กรอกข้อมูล" class="w-full" />
+                </UFormField>
+              </UFormField> 
+              <UFormField v-if="index!=0" :error="errors.fiscalYear" required>
+                <template #label>
+                  <span class="text-[1rem] text-black">{{ item.label }}</span>
+                </template>
+                <UFormField :error="item.error">
+                  <UInput placeholder="กรอกข้อมูล" class="w-full" />
+                </UFormField>
+              </UFormField> 
+
+            </div>
+
+            </div>
+          </div>
         </div>
       </UCard>
     </div>
@@ -181,6 +281,7 @@
 import { reactive } from "vue";
 import DatePicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
+
 // กำหนดข้อมูลปีงบประมาณ
 const years = [
   { label: "2565", value: 2565 },
@@ -188,10 +289,18 @@ const years = [
   { label: "2567", value: 2567 },
 ];
 
+// ข้อมูลตัวเลือกสำหรับชั้นความเร็ว
+const urgent = [
+  { label: "ด่วน", value: "URGENT" },
+  { label: "ด่วนมาก", value: "VERY_URGENT" },
+  { label: "ด่วนที่สุด", value: "MOST_URGENT" },
+];
+
 // สถานะของฟอร์ม
 const formState = reactive({
-  fiscalYear: null, // ค่าเริ่มต้น
+  fiscalYear: null,
   announceDate: null,
+  urgentLevel: "NORMAL", // ค่าเริ่มต้นของชั้นความเร็ว
 });
 
 // ตัวอย่างการจัดการข้อผิดพลาด
@@ -203,7 +312,12 @@ const items = [
   { label: "เลขที่รับ ชภ.", error: "ห้ามเว้นว่างเลขที่รับ ชภ." },
   { label: "เลขที่รับ สล.", error: "ห้ามเว้นว่างเลขที่รับ สล." },
 ];
-
+const items2 = [
+  { label: "จำนวนเงินขออนุมัติ ", error: "กรุณากรอกข้อมูล" },
+  { label: "จำนวนเงินทั้งหมด ตามจังหวัด", error: "กรุณากรอกข้อมูล" },
+  { label: "จำนวนเงินทั้งหมด ตามครั้งที่ขอ", error: "กรุณากรอกข้อมูล" },
+  { label: "จำนวนเงินทั้งหมด ตามที่ขอ", error: "กรุณากรอกข้อมูล" },
+];
 definePageMeta({
   title: "ระบบตรวจติดตาม",
 });
